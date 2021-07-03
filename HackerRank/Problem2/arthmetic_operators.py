a4771b32e7fc962e5f331aa5d45d29168536328e
Yash BRAHMANI <yashbrahmani.nmims@gmail.com>
2021-07-03 19:27:43 +0530

    hackerRank python Problem 2 solved

---
+++ HackerRank/Problem2/arthmetic_operators.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    a = int(input());
+    b = int(input());
+    print(f"{a+b}");
+    print(f"{a-b}");
+    print(f"{a*b}");
+    # another way to print the above is as below
+#    print(f"Sum of {a} and {b} is {a+b}");
+#    print(f"Subtraction of {a} and {b} is {a-b}");
+#    print(f"Multiplication of {a} and {b} is {a*b}");
