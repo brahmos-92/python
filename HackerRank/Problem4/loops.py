eaeaae0e033c34f371c18a8ae04e7a4c1a16c408
Yash BRAHMANI <yashbrahmani.nmims@gmail.com>
2021-07-03 21:05:53 +0530

    HackerRank python Problem 5 solved

---
+++ HackerRank/Problem4/loops.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    while (n>=0 and n<=20):
+      for i in range(0,n):
+            print(f"{pow(i,2)}");
+      break
